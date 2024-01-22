faabec31d52279672d493d593b5ea9b800785aaa
raghu <raghuciet@gmail.com>
2024-01-22 12:26:15 +0530

    shouldReturnZeroOnEmptyString

--- src/test/java/com/stringCalculator/CalculatorTest.java
+++ src/test/java/com/stringCalculator/CalculatorTest.java
@@ -1,5 +1,21 @@
 package com.stringCalculator;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import stringcctdd.StringCalculator;
+
 public class CalculatorTest {
+	
+	//	Task - 1: Create a simple String calculator with a method signature:
+	//	———————————————
+	//	int Add(string numbers)
+	//	———————————————
+	// TODO: for empty string it will return zero
+	@Test
+	public void shouldReturnZeroOnEmptyString() {
+		assertEquals(0, StringCalculator.add(""));
+	}
 
 }
